ccc3250f15aff5c85fc7abed4bff326a611a1226
Marola <126696230+Maarola@users.noreply.github.com>
2024-03-07 19:54:48 -0300

    att
    primeira parte finalizada.

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+function imgSlider(e){
+    document.querySelector('#phone').src = e;
+}
+
+function mudarCirculo(color){
+    const circle = document.querySelector('.circulo');
+
+    circle.style.background = color;
+}
